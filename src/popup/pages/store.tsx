73c373b53bf8cb773e5423c6d4ff533a355d0c3e
eulerthedestroyer <eulerthedestroyer@gmail.com>
2021-09-04 12:57:22 -0700

    worked on store

--- src/popup/pages/store.tsx
+++ src/popup/pages/store.tsx
@@ -1,7 +1,35 @@
 import React from "react"
-const store: React.FC = ()=>{
+import { useSelector } from "react-redux"
+import { RootState } from "../../redux/store"
+
+
+class Upgrade {
+    constructor(public name: string, public cost: number, public component: React.FC) { }
+}
+
+const betterButton = new Upgrade("better button", 10, () => <>hello</>)
+const upgrades: Array<Upgrade> = [betterButton]
+
+function getPossibleUpgrades(money: number): Array<Upgrade> {
+    return upgrades.filter(upgrade => upgrade.cost < money)
+}
+
+interface PossiblePurchaseProps {
+    name: string;
+    cost: number;
+}
+const PossiblePurchase: React.FC<PossiblePurchaseProps> = (props) => {
     return (<>
-    I am store
+        <h1>{props.name}</h1>
+        <button>buy for {props.cost}</button>
+    </>)
+}
+
+const store: React.FC = () => {
+    const money = useSelector<RootState>((state) => state.money.value) as number
+    return (<>
+        I am store
+        {getPossibleUpgrades(money).map(e => <PossiblePurchase {...e} />)}
     </>)
 }
 export default store;
