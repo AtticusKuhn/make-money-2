3dcf359f9d4d3aacf14b582c739d0e4d849aa549
eulerthedestroyer <eulerthedestroyer@gmail.com>
2021-10-23 19:16:28 -0700

    added tabs

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom';
 import { storageUpgrade } from '../redux/earn';
 import { RootState } from '../redux/store';
 import { toCss } from '../upgrades/upgrades';
+import { useLocation } from 'react-router-dom'
+
 import './App.scss';
 import Debug from './pages/debug';
 import Index from './pages/Index';
@@ -15,7 +17,17 @@ import { casino } from '../upgrades/upgrades';
 import Casino from './pages/casino';
 import Club from './pages/club';
 import Tutorial from './pages/tutorial';
-const App = () => {
+interface NavButtonProps {
+    text: string;
+    link: string;
+}
+const NavButton: React.FC<NavButtonProps> = ({ text, link }) => {
+    const location = useLocation()
+    return <div className={`navbutton ${location.pathname === link ? "active" : "inactive"}`}><li>
+        <Link to={link}>{text}</Link>
+    </li></div>;
+}
+const App: React.FC<{}> = () => {
     const l = findPossibleUpgrades().length;
     const msg = l > 0 ? `(${l} upgade${l > 1 ? "s" : ""})` : ""
     const equipped = useSelector<RootState, storageUpgrade[]>(state => state.money.equippedUpgrades)
@@ -27,48 +39,24 @@ const App = () => {
     return <div id="app" className={`app ${cssString}`}>
         <div className="content">
             <div className="navbar">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/LoadOut">Load Out</Link>
-                </li>
-                <li>
-                    <Link to="/store">Store {msg}</Link>
-                </li>
-                <li>
-                    <Link to="/debug">Debug</Link>
-                </li>
-                {
-                    isTutorial
-                    &&
-                    <li>
-                        <Link to="/tutorial">Tutorial</Link>
-                    </li>
-                }
-                {
-                    isCasino
-                    &&
-                    <li>
-                        <Link to="/casino">Casino</Link>
-                    </li>
-                }
-                {
-                    isBillionaire
-                    &&
-                    <li>
-                        <Link to="/club">Billionaire Club</Link>
-                    </li>
-                }
+                <NavButton link="/" text="Home" />
+                <NavButton link="/LoadOut" text="Load Out" />
+                <NavButton link="/store" text={`Store ${msg}`} />
+                <NavButton link="/debug" text="debug" />
+                {isTutorial && <NavButton link="/tutorial" text="Tutorial" />}
+                {isCasino && <NavButton link="/casino" text="Casino" />}
+                {isBillionaire && <NavButton link="club" text="Billionaire Club" />}
             </div>
             <br />
-            <Route exact path="/" component={Index} />
-            <Route path="/LoadOut" component={LoadOut} />
-            <Route path="/debug" component={Debug} />
-            <Route path="/store" component={store} />
-            <Route path="/tutorial" component={Tutorial} />
-            <Route path="/casino" component={Casino} />
-            <Route path="/club" component={Club} />
+            <div className="body">
+                <Route exact path="/" component={Index} />
+                <Route path="/LoadOut" component={LoadOut} />
+                <Route path="/debug" component={Debug} />
+                <Route path="/store" component={store} />
+                <Route path="/tutorial" component={Tutorial} />
+                <Route path="/casino" component={Casino} />
+                <Route path="/club" component={Club} />
+            </div>
         </div>
     </div>;
 };
