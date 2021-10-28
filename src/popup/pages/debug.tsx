c342ac1648000ea0b8e6555dba5f620d8cf3c08d
eulerthedestroyer <eulerthedestroyer@gmail.com>
2021-10-27 18:21:05 -0700

    added more info to debug

--- src/popup/pages/debug.tsx
+++ src/popup/pages/debug.tsx
@@ -14,9 +14,10 @@ const Debug: React.FC<{}> = () => {
         <h1>Developer Debugger</h1>
         <p>Do you want to experience the joy of Make Money again? If you reset, all your items
             will go away and you will go to the beginning of the story. If you prestige, you
-            lose your items but your income increases. Prestiging can be a good way to get
-            past a rough spot where you are stuck.
+            lose your items but your income increases.<b> Prestiging can be a good way to get
+                past a rough spot where you are stuck.</b>
         </p>
+        <p>If you prestiege your income increases by your the square root of your money times 0.0001</p>
         <button onClick={() => dispatch(reset())}>reset all</button>
         <br />
         <br />
