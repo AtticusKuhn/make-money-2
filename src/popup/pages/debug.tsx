a139e85c028369c0952fe5f453102ae0a5cc40c1
eulerthedestroyer <eulerthedestroyer@gmail.com>
2021-11-24 19:59:41 -0800

    added hover to button

--- src/popup/pages/debug.tsx
+++ src/popup/pages/debug.tsx
@@ -11,7 +11,7 @@ const Debug: React.FC<{}> = () => {
     const dispatch = useDisp()
     // let r = useRef<HTMLInputElement>(null)
     const [v, setV] = useState<number>(100)
-    return (<>
+    return (<div className="btn-center">
         <h1>Developer Debugger</h1>
         <p>Do you want to experience the joy of Make Money again? If you reset, all your items
             will go away and you will go to the beginning of the story. If you prestige, you
@@ -22,19 +22,27 @@ const Debug: React.FC<{}> = () => {
         <br />
         <br />
         <br />
-        <div className="btn-center"><button className="btn" style={{ backgroundColor: "red", }} onClick={() => dispatch(reset())}>reset all</button>
+        <div className="btn-center"><button id="reset" className="btn" onClick={() => dispatch(reset())}>reset all</button>
         </div>
+        <style>{`#reset:hover{ background-color:red !important;}`}</style>
         (lose everything and start from beginning)
         <br />
         <br />
         <br />
-        <button disabled={money < 1e6} onClick={() => dispatch(prestige())}>prestige</button>
+        <div className="btn-center">
+            <button className="btn" id="prestiege" disabled={money < 1e6} onClick={() => dispatch(prestige())}>prestige</button>
+        </div>
+        <style>{`#prestiege:hover{ background-color:red !important;;}`}</style>
+
         (your income will be ${Math.floor(income + Math.sqrt(money) * 0.0001)} )
         <br />
         <br />
         <br />
         <input value={v} onChange={(e) => isNaN(Number(e.target.value)) || setV(Number(e.target.value))} />
-        <button onClick={() => dispatch(set(Number(v)))}>set your money to ${v}</button>
-    </>)
+        <br /> <br />
+        <div className="btn-center">
+            <button className="btn" onClick={() => dispatch(set(Number(v)))}>set your money to ${v}</button>
+        </div>
+    </div>)
 }
 export default Debug;
