7a5e737680a00617cc21d86d7457957462acd2d3
eulerthedestroyer <eulerthedestroyer@gmail.com>
2021-11-24 19:41:44 -0800

    changed casino

--- src/popup/pages/casino.tsx
+++ src/popup/pages/casino.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react"
 import { simpleEarn } from "../../redux/earn"
 import { useDisp, useSel } from "../../redux/store"
-import { fromMaybe, inRange, maybeParseInt, randomInRange, sleep } from "../../utils"
+import { formatNumber, fromMaybe, inRange, maybeParseInt, randomInRange, sleep } from "../../utils"
 
 const SlotNumber: React.FC<{ number: number }> = ({ number }) => {
     return <div style={{ margin: "0px", border: "1px solid black", fontSize: "40px", display: "inline-block", width: "40px", height: "50px" }}>
@@ -66,111 +66,110 @@ const SlotMachine: React.FC<{}> = () => {
         dispatch(simpleEarn(bet * res.multiplier))
         setMsg(res.msg)
     }
-    return <div style={{ alignItems: "center", alignContent: "center" }}>
+    return <div className="btn-center" style={{ alignItems: "center", alignContent: "center" }}>
         <h2>Slot Machine</h2> <br /> <br /><br /><br /><br />
         {val.map((v, i) => <SlotNumber key={i} number={v} />)} <br /><br /><br /><br />
         <input ref={ref} placeholder="amount to gamble" /> <br /><br />
         {msg} <br /><br /><br />
-        <button disabled={money === 0 || spinning} onClick={click}>Gamble Slot Machine</button><br /><br /><br /><br />
+        <button className="btn" disabled={money === 0 || spinning} onClick={click}>Gamble Slot Machine</button><br /><br /><br /><br />
     </div>
 }
 const RouletteWheel: React.FC<{}> = () => {
     // const money = useSel(state => state.money.value)
     const dispatch = useDisp()
     const radius = 120;
+    var options: number[] = new Array(10).fill(0).map((_e, i) => Math.floor((i - 5)));
+    var startAngle = 0;
+    var arc = Math.PI / (options.length / 2);
+    var spinTimeout: number = 0;
+    var spinTime = 0;
+    var spinTimeTotal = 0;
+    var ctx: CanvasRenderingContext2D;
+    const getColor = (item: number, maxItem: number): string => `hsl(${item * 360 / maxItem},70%, 50%)`;
+    function drawRouletteWheel() {
+        var canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        if (canvas.getContext) {
+            var outsideRadius = radius + 10;
+            var textRadius = radius - 30;
+            var insideRadius = radius / 2;
+            ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+            ctx.clearRect(0, 0, 2 * radius, 2 * radius);
+            ctx.strokeStyle = "black";
+            for (var i = 0; i < options.length; i++) {
+                var angle = startAngle + i * arc;
+                ctx.fillStyle = getColor(i, options.length);
+                ctx.beginPath();
+                ctx.arc(radius + 10, radius + 10, outsideRadius, angle, angle + arc, false);
+                ctx.arc(radius + 10, radius + 10, insideRadius, angle + arc, angle, true);
+                ctx.stroke();
+                ctx.fill();
+                ctx.save();
+                ctx.shadowOffsetX = -1;
+                ctx.shadowOffsetY = -1;
+                ctx.shadowBlur = 0;
+                ctx.shadowColor = "rgb(220,220,220)";
+                ctx.fillStyle = "black";
+                ctx.translate(radius + Math.cos(angle + arc / 2) * textRadius,
+                    radius + Math.sin(angle + arc / 2) * textRadius);
+                ctx.rotate(angle + arc / 2 + Math.PI / 2);
+                var text = options[i];
+                ctx.font = 'bold 15px Helvetica, Arial';
+                ctx.fillText(`${text}%`, -ctx.measureText(text.toString()).width / 2, 0);
+                ctx.restore();
+            }
+        }
+    }
+    let spinAngleStart = 0;
+    function spin() {
+        spinAngleStart = Math.random() * 10 + 10;
+        spinTime = 0;
+        spinTimeTotal = Math.random() * 3 + 4 * 1000;
+        rotateWheel();
+    }
+    function rotateWheel() {
+        spinTime += 30;
+        if (spinTime >= spinTimeTotal) {
+            stopRotateWheel();
+            return;
+        }
+        var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
+        startAngle += (spinAngle * Math.PI / 180);
+        drawRouletteWheel();
+        //@ts-ignore
+        spinTimeout = setTimeout(() => rotateWheel(), 30);
+    }
+    function stopRotateWheel() {
+        clearTimeout(spinTimeout);
+        var degrees = startAngle * 180 / Math.PI + 90;
+        var arcDegrees = arc * 180 / Math.PI;
+        var index = Math.floor((360 - degrees % 360) / arcDegrees);
+        ctx.save();
+        var text = options[index]
+        ctx.font = 'bold 15px Helvetica, Arial';
+        ctx.fillText(`${text}%`, radius - ctx.measureText(text.toString()).width / 2, radius + 10);
+        dispatch(simpleEarn(text))
+        ctx.restore();
+    }
+    function easeOut(t: number, b: number, c: number, d: number): number {
+        var ts = (t /= d) * t;
+        var tc = ts * t;
+        return b + c * (tc + -3 * ts + 3 * t);
+    }
     useEffect(() => {
-        var options: number[] = new Array(10).fill(0).map((_e, i) => Math.floor((i - 5)));
-        var startAngle = 0;
-        var arc = Math.PI / (options.length / 2);
-        var spinTimeout: number = 0;
-        var spinTime = 0;
-        var spinTimeTotal = 0;
-        var ctx: CanvasRenderingContext2D;
-        document.getElementById("spin")!.addEventListener("click", spin);
-        const getColor = (item: number, maxItem: number): string => `hsl(${item * 360 / maxItem},70%, 50%)`;
-        function drawRouletteWheel() {
-            var canvas = document.getElementById("canvas") as HTMLCanvasElement;
-            if (canvas.getContext) {
-                var outsideRadius = radius + 10;
-                var textRadius = radius - 30;
-                var insideRadius = radius / 2;
-                ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-                ctx.clearRect(0, 0, 2 * radius, 2 * radius);
-                ctx.strokeStyle = "black";
-                for (var i = 0; i < options.length; i++) {
-                    var angle = startAngle + i * arc;
-                    ctx.fillStyle = getColor(i, options.length);
-                    ctx.beginPath();
-                    ctx.arc(radius + 10, radius + 10, outsideRadius, angle, angle + arc, false);
-                    ctx.arc(radius + 10, radius + 10, insideRadius, angle + arc, angle, true);
-                    ctx.stroke();
-                    ctx.fill();
-                    ctx.save();
-                    ctx.shadowOffsetX = -1;
-                    ctx.shadowOffsetY = -1;
-                    ctx.shadowBlur = 0;
-                    ctx.shadowColor = "rgb(220,220,220)";
-                    ctx.fillStyle = "black";
-                    ctx.translate(radius + Math.cos(angle + arc / 2) * textRadius,
-                        radius + Math.sin(angle + arc / 2) * textRadius);
-                    ctx.rotate(angle + arc / 2 + Math.PI / 2);
-                    var text = options[i];
-                    ctx.font = 'bold 15px Helvetica, Arial';
-                    ctx.fillText(`${text}%`, -ctx.measureText(text.toString()).width / 2, 0);
-                    ctx.restore();
-                }
-            }
-        }
-        let spinAngleStart = 0;
-        function spin() {
-            spinAngleStart = Math.random() * 10 + 10;
-            spinTime = 0;
-            spinTimeTotal = Math.random() * 3 + 4 * 1000;
-            rotateWheel();
-        }
-        function rotateWheel() {
-            spinTime += 30;
-            if (spinTime >= spinTimeTotal) {
-                stopRotateWheel();
-                return;
-            }
-            var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
-            startAngle += (spinAngle * Math.PI / 180);
-            drawRouletteWheel();
-            //@ts-ignore
-            spinTimeout = setTimeout(() => rotateWheel(), 30);
-        }
-        function stopRotateWheel() {
-            clearTimeout(spinTimeout);
-            var degrees = startAngle * 180 / Math.PI + 90;
-            var arcDegrees = arc * 180 / Math.PI;
-            var index = Math.floor((360 - degrees % 360) / arcDegrees);
-            ctx.save();
-            var text = options[index]
-            ctx.font = 'bold 15px Helvetica, Arial';
-            ctx.fillText(`${text}%`, radius - ctx.measureText(text.toString()).width / 2, radius + 10);
-            dispatch(simpleEarn(text))
-            ctx.restore();
-        }
-        function easeOut(t: number, b: number, c: number, d: number): number {
-            var ts = (t /= d) * t;
-            var tc = ts * t;
-            return b + c * (tc + -3 * ts + 3 * t);
-        }
         drawRouletteWheel();
     }, [])
-    return <>
+    return <div className="btn-center">
         <h1>Roulette Wheel</h1>
-
         <canvas id="canvas" width={(2 * radius + 20).toString()} height={(2 * radius + 20).toString()} />
-        <input type="button" value="spin the wheel" style={{ float: "left" }} id='spin' />
-    </>
+        <br /> <br />
+        <button onClick={spin} className="btn" type="button" value="spin the wheel" style={{ float: "left" }} id='spin'>Spin the Wheel!</button>
+    </div>
 }
 const Casino: React.FC<{}> = () => {
     const money = useSel(state => state.money.value)
     return (<>
         <h1>Casino</h1>  <br />
-        get ready to gamble (${money}) <br />
+        get ready to gamble (${formatNumber(money)}) <br />
         <div className="btn-center">
             <SlotMachine />
         </div>
